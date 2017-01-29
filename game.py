2e5ac0bfae45807b138b75f537ef9d04c545106d
Gurpreet Singh <gurpreetsk@hotmail.com>
2017-01-29 13:44:14 +0530

    - game script

--- game.py
+++ game.py
@@ -8,4 +8,4 @@ observation_n = env.reset()
 while True:
     action_n = [[('KeyEvent', 'ArrowUp', True)] for ob in observation_n]  # your agent here
     observation_n, reward_n, done_n, info = env.step(action_n)
-    # env.render()
+    env.render()
